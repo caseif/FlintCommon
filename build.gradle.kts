71345774c2c956bdd119ba8fbbd6d47108e04ff9
Max Roncace <me@caseif.net>
2022-03-17 23:50:33 -0400

    Convert build script to Kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,147 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import blue.lapis.methodremapper.gradle.RemapTask
+
+plugins {
+    java
+    `maven-publish`
+    eclipse
+    idea
+    checkstyle
+    id("org.cadixdev.licenser") version "0.6.1"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("net.caseif.methodremapper") version "0.3"
+}
+
+defaultTasks("clean", "updateLicenses", "build", "shadowJar")
+
+// Project information
+group = "net.caseif.flint.common"
+version = "1.3.6"
+description = "Code shared across implementations of Flint."
+
+// Extended project information
+val projectName by extra { "flintcommon" }
+val inceptionYear by extra { "2015" }
+val packaging by extra { "jar" }
+val author by extra { "Max Roncace" }
+val flintVersion by extra { "1.3.2" }
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_7
+    targetCompatibility = JavaVersion.VERSION_1_7
+}
+
+// Project repositories
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven("https://repo.caseif.net/content/groups/public/")
+}
+
+// Project dependencies
+dependencies {
+    shadow("com.google.guava:guava:17.0")
+    shadow("com.google.code.gson:gson:2.2.4")
+    implementation("net.caseif.flint:flint:" + flintVersion)
+}
+
+// Read source files using UTF-8
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
+
+tasks.named<Copy>("processResources") {
+    from("LICENSE")
+}
+
+// License header formatting
+license {
+    include("**/*.java")
+    ignoreFailures.set(false)
+}
+
+// check code style
+checkstyle {
+    configFile = file("etc/checkstyle.xml")
+}
+
+tasks.withType<Checkstyle> {
+    exclude("**/*.properties")
+    exclude("**/*.yml")
+}
+
+tasks.withType<Jar> {
+    classifier = "base"
+    manifest {
+        attributes["Created-By"] = System.getProperty("java.vm.version") + " (" + System.getProperty("java.vm.vendor") + ")"
+        attributes["Specification-Title"] = "Flint"
+        attributes["Specification-Version"] = flintVersion
+        attributes["Specification-Vendor"] = "Max Roncace"
+        attributes["Implementation-Title"] = name
+        attributes["Implementation-Version"] = version
+        attributes["Implementation-Vendor"] = author
+    }
+}
+
+tasks.withType<ShadowJar> {
+    dependencies {
+        exclude(dependency("com.google.guava:guava"))
+        exclude(dependency("com.google.code.gson:gson"))
+        exclude(dependency("org.bukkit:bukkit"))
+    }
+    classifier = ""
+
+    finalizedBy(tasks.remap)
+}
+
+tasks.create<Jar>("sourceJar") {
+    from(sourceSets["main"].java)
+    from(sourceSets["main"].resources)
+    classifier = "sources"
+}
+
+tasks.named<RemapTask>("remap") {
+    inputJar = (tasks["shadowJar"] as Jar).archiveFile.get().getAsFile()
+    config = project.file("etc/remap.txt")
+}
+
+artifacts {
+    archives(tasks["shadowJar"])
+    archives(tasks["sourceJar"])
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+
+            pom {
+                groupId = project.group as String
+                version = project.version as String
+                artifactId = projectName
+
+                packaging = packaging
+                description.set(description)
+                url.set("http://github.com/caseif/FlintCommon")
+
+                scm {
+                    url.set("https://github.com/caseif/FlintCommon")
+                    connection.set("scm:git:git://github.com/caseif/FlintCommon.git")
+                    developerConnection.set("scm:git:git@github.com:caseif/FlintCommon.git")
+                }
+
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("http://opensource.org/licenses/MIT")
+                        distribution.set("repo")
+                    }
+                }
+            }
+        }
+    }
+}
+
+tasks.withType<Wrapper> {
+    gradleVersion = "7.4.1"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+pluginManagement {
+    repositories {
+        maven("https://repo.caseif.net")
+        gradlePluginPortal()
+    }
+}
+
+rootProject.name = "flintcommon"
